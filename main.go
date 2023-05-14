6e4ea797002415ed9cdefa957b21ad6b59028190
zishan044 <winchesterfelix007@gmail.com>
2023-05-15 00:18:02 +0600

    routes defined

--- main.go
+++ main.go
@@ -1,19 +1,44 @@
 package main
 
 import (
-	"net/http"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
+
+type Books struct {
+	Author    string `json:"author"`
+	Title     string `json:"title"`
+	Publisher string `json:"publisher"`
+}
+
+type Repository struct {
+	DB *gorm.DB
+}
+
+func (r *Repository) SetupRoutes(app *fiber.App) {
+	api := app.Group("/api")
+
+	api.Get("/books", r.GetBooks)
+	api.Get("/books/:id", r.GetBookByID)
+	api.Post("/books", r.CreateBook)
+	api.Delete("/books/:id", r.DeleteBook)
+}
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		c.Status(http.StatusOK).JSON(&fiber.Map{"msg": "app running succesfully"})
-		return nil
-	})
+	db, err := storage.NewConnection(config)
+	if err != nil {
+		log.Fatal("could not load database")
+	}
 
+	r := &Repository{
+		DB: db,
+	}
+
+	r.SetupRoutes(app)
 	app.Listen(":8000")
 
 }
