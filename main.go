f3c6e06e58009aae1768081af24637b44dd85f8c
zishan044 <winchesterfelix007@gmail.com>
2023-05-15 01:06:58 +0600

    GetBooks and CreateBook function implemented

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
 
-type Books struct {
+type Book struct {
 	Author    string `json:"author"`
 	Title     string `json:"title"`
 	Publisher string `json:"publisher"`
@@ -24,6 +25,42 @@ func (r *Repository) SetupRoutes(app *fiber.App) {
 	api.Get("/books/:id", r.GetBookByID)
 	api.Post("/books", r.CreateBook)
 	api.Delete("/books/:id", r.DeleteBook)
+}
+
+func (r *Repository) CreateBook(c *fiber.Ctx) error {
+	book := Book{}
+	if err := c.BodyParser(&book); err != nil {
+		c.Status(http.StatusUnprocessableEntity).JSON(&fiber.Map{
+			"msg": "request failed",
+		})
+		return err
+	}
+
+	if err := r.DB.Create(&book).Error; err != nil {
+		c.Status(http.StatusBadRequest).JSON(&fiber.Map{
+			"msg": "could not create book",
+		})
+		return err
+	}
+
+	c.Status(http.StatusOK).JSON(&fiber.Map{
+		"msg": "succesfully created book",
+	})
+	return nil
+}
+
+func (r *Repository) GetBooks(c *fiber.Ctx) error {
+	books := &[]models.Book{}
+	if err := r.DB.Find(books).Error; err != nil {
+		c.Status(http.StatusBadRequest).JSON(&fiber.Map{
+			"msg": "could not fetch books",
+		})
+	}
+	c.Status(http.StatusOK).JSON(&fiber.Map{
+		"msg":  "books fetched successfully",
+		"data": books,
+	})
+	return nil
 }
 
 func main() {
